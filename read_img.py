8d8f040a71441b14d41ae836349256ebd57141d0
Kian <sabbah128@gmail.com>
2022-04-27 01:07:33 +0430

    read & write
    read from main folder and resized 128*128 and convert to jpeg

---
+++ read_img.py
@@ -0,0 +1,36 @@
+import os
+import pydicom
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from PIL import Image
+
+
+path='E:\\Test-programing\\JIA\\get_images\\dicom_img'
+names, img_path = [], []
+
+
+for (root, dirs, file) in os.walk(path):
+    for f in file:
+        if '.IMA' in f:
+            names.append(f.split('.')[0])
+            img_path.append(root+"\\"+f)     
+
+folder = 'Images\\'
+for i in range(len(img_path)):
+    img = pydicom.dcmread(img_path[i])
+    img = img.pixel_array
+    img_shape = img.shape
+
+    if len(img_shape) == 3 and img_shape[0] == 2 and (img_shape[1] == 128 or img_shape[1] == 256):
+        plt.imsave(folder + names[i] + str(i) + '1.jpeg', img[0], cmap=cm.gray)
+        plt.imsave(folder + names[i] + str(i) + '2.jpeg', img[1], cmap=cm.gray)
+        if img_shape[1] == 256:
+            im1 = Image.open(folder + names[i] + str(i) + '1.jpeg')
+            im2 = Image.open(folder + names[i] + str(i) + '1.jpeg')
+            im1 = im1.resize((128, 128))
+            im2 = im2.resize((128, 128))
+            im1.save(folder + names[i] + str(i) + '1.jpeg')
+            im2.save(folder + names[i] + str(i) + '2.jpeg')
+    else:
+        img_path[i] = 'null'
